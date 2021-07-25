be43386f8e2c845c2b2435ad887ff18f527d5977
Damian Karczmarz <damian.karczmarz92@gmail.com>
2021-07-25 12:01:23 +0200

    init

---
+++ src/main/java/pl/coderslab/entity/User.java
@@ -0,0 +1,18 @@
+package pl.coderslab.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+}
